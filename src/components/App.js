b704c37d9fdb4def9e2088690016587794057a03
AMIT KUMAR RAY <110167023+AMITRAY957@users.noreply.github.com>
2022-10-09 16:09:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,22 @@ import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
+ //code here
  const[name,setName]=
-       useState(");
+       useState([]);
     const[id,setId]=useState(1)
-//code here 
+
  useEffect(()=>{
   fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`)
-  .then((resp)=>resp.json()).then((data)
-   =>{
+  .then(res=>res.json())
+   .then(data)=>{
+   
    console.log(data.name);
    setName(data.name)
   })
  },[id])
  const changeInput=(e)=>{
-  setld(e.target.value)
+  setid(e.target.value)
  }
  
 
